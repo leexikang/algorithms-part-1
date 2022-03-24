b2422392219ee34fec915615a1b6402592911a8d
min <minsan.work@gmail.com>
2022-03-25 07:20:19 +0800

    setup project

--- 8puzzle/PuzzleChecker.java
+++ 8puzzle/PuzzleChecker.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  Compilation:  javac-algs4 PuzzleChecker.java
  *  Execution:    java-algs4 PuzzleChecker filename1.txt filename2.txt ...
@@ -51,4 +52,78 @@ public class PuzzleChecker {
             StdOut.println(filename + ": " + solver.moves());
         }
     }
+}
+
+public class Board {
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+
+    }
+
+    // string representation of this board
+    public String toString() {
+    }
+
+    // board dimension n
+    public int dimension() {
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+    }
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+    }
+
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+    }
+
+    // unit testing (not graded)
+    public static void main(String[] args) {
+    }
+
+}
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+    }
+
 }
